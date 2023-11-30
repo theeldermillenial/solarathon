468f7169eda3b2c4309ecdd53cfcde50951a70cc
agora <adrian.gora14@gmail.com>
2023-11-30 16:48:12 +0100

    feat: load icon from base64 string

--- solarathon/pages/tokenregistry.py
+++ solarathon/pages/tokenregistry.py
@@ -9,10 +9,15 @@ Token Registry
 from concurrent.futures import ThreadPoolExecutor
 import json
 import solara
+from solara.alias import rv
 
 from pathlib import Path
 from typing import Any, Dict, Optional, cast
 import pandas as pd
+from PIL import Image
+import base64
+from io import BytesIO
+
 
 open_dialog = solara.reactive(False)
 
@@ -41,10 +46,24 @@ def batchMetadataQuery():
 
     return list(t for t in tokens if t is not None)
 
-
 # TODO: Create a custom modal that opens when you want to select tokens that shows icon, ticker name, and a selection
 # @solara.component
 # def CryptoModal():
+
+@solara.component
+def GeckoIcon(name: str, img: str):
+    with solara.v.Html(
+        tag="a",
+        attributes={"href": f"https://www.binance.com/en/trade", "target": "_blank"},
+    ):
+        with solara.v.ListItem(class_="pa-0"):
+            with solara.v.ListItemAvatar(color="white"):
+                solara.v.Img(
+                    class_="elevation-6",
+                    src=img,
+                )
+            with solara.v.ListItemContent():
+                solara.v.ListItemTitle(children=[name], style_=f"color: white")
 
 
 @solara.component
@@ -54,22 +73,38 @@ def Page():
     content, set_content = solara.use_state(None)
     is_open, set_is_open = solara.use_state(False)
 
-    metadata, _ = solara.use_state(batchMetadataQuery())
-    index = 0
+    metadata = solara.use_memo(lambda: batchMetadataQuery(), [])
 
     # Create a dictionary with token policy+name as keys and ticker name and icon as values
     token_info_dict = {}
-    for subject in metadata:
+    for index, subject in enumerate(metadata):
         policy = subject["policy"]
         name_value = subject["name"]["value"]
-        ticker_value = None if subject["ticker"] is None else subject["ticker"]["value"]
-        icon_value = None if subject["logo"] is None else subject["logo"]["value"]
+        ticker_value = subject["ticker"]["value"] if subject["ticker"] is not None else None
+        icon_value = subject["logo"]["value"] if subject["logo"] is not None else None
+
+        # LOAD ICONS - heavy
+        # if icon_value:
+        #     try:
+        #         # Decode the Base64 string into bytes
+        #         decoded = base64.b64decode(icon_value)
+        #         # Open the image using PIL from the decoded bytes
+        #         img = Image.open(BytesIO(decoded))
+        #         # Convert the image to HTML format and store it
+        #         if isinstance(img, Image.Image):
+        #             buffered = BytesIO()
+        #             img.save(buffered, format="PNG")
+        #             img_str = base64.b64encode(buffered.getvalue()).decode()
+        #             icon_value = f'<img src="data:image/png;base64,{img_str}" width="20px" height="20px">'
+        #     except Exception as e:
+        #         print(f"Failed to process image: {e}")
+        #         icon_value = None
+
         token_info_dict[f"{policy}-{name_value}"] = {
             "index": index,
             "ticker": ticker_value,
             "icon": icon_value,
         }
-        index += 1
 
     def on_close():
         set_is_open(False)
@@ -110,11 +145,28 @@ def Page():
         # Get familiar with Cardano Token Registry
     """
     )
+    
+    # def render_html(val):
+    #     return solara.HTML(tag="div", unsafe_innerHTML=val) if val else None
+
     df = pd.DataFrame.from_dict(token_info_dict, orient="index")
-    df.index.name = "Token Policy+Name"
+
+    # inject html into dataframe
+    # df["Icon"] = df["icon"].apply(render_html)
+
     df.reset_index(inplace=True)
 
-    solara.DataFrame(df, column_actions=column_actions, cell_actions=cell_actions)
+
+    # cols = df.columns.tolist()
+    # cols = ['Icon'] + [col for col in cols if col != 'Icon']
+    # df = df[cols]
+
+    solara.DataFrame(
+        df, 
+        column_actions=column_actions, 
+        cell_actions=cell_actions,
+    )
+
     solara.lab.ConfirmationDialog(
         is_open, ok="Ok, Cancel", on_ok=on_close, content=f"Content: {content}"
     )
